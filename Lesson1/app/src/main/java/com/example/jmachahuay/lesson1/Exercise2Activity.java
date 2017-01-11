2e8d15aafc52f40300ba3ce81be80b2c385405ea
johnprog <john.cfmr.2009@gmail.com>
2017-01-11 17:44:40 -0500

    Added solution for the second and third exercise.

--- Lesson1/app/src/main/java/com/example/jmachahuay/lesson1/Exercise2Activity.java
+++ Lesson1/app/src/main/java/com/example/jmachahuay/lesson1/Exercise2Activity.java
@@ -2,12 +2,68 @@ package com.example.jmachahuay.lesson1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Exercise2Activity extends AppCompatActivity {
+
+    EditText firstNumber, secondNumber, operator;
+    TextView txtResult;
+    Button btnCalculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise2);
+
+        firstNumber = (EditText) findViewById(R.id.edtFirstNumber);
+        secondNumber = (EditText) findViewById(R.id.edtSecondNumber);
+        operator = (EditText) findViewById(R.id.edtOperator);
+        btnCalculate = (Button) findViewById(R.id.btnCalculate);
+        txtResult = (TextView) findViewById(R.id.txtResult);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstNumberText = firstNumber.getText().toString(),
+                        secondNumberText = secondNumber.getText().toString(),
+                        operatorText = operator.getText().toString();
+                Double result;
+
+                try {
+                    Double  txtFirstNumber = Double.parseDouble(firstNumberText),
+                        txtSecondNumber = Double.parseDouble(secondNumberText);
+
+                    switch (operatorText) {
+                        case "+":
+                            result = txtFirstNumber + txtSecondNumber;
+                            break;
+                        case "-":
+                            result = txtFirstNumber - txtSecondNumber;
+                            break;
+                        case "*":
+                            result = txtFirstNumber * txtSecondNumber;
+                            break;
+                        case "/":
+                            result = txtFirstNumber / txtSecondNumber;
+                            break;
+                        default:
+                            result = null;
+                            break;
+                    }
+
+                    if (result != null) {
+                        txtResult.setText(Double.toString(result));
+                    } else {
+                        txtResult.setText(R.string.txtValidOperator);
+                    }
+                }
+                catch (NumberFormatException ex) {
+                    txtResult.setText(R.string.txtValidNumber);
+                }
+            }
+        });
     }
 }
--- Lesson1/app/src/main/java/com/example/jmachahuay/lesson1/Exercise3Activity.java
+++ Lesson1/app/src/main/java/com/example/jmachahuay/lesson1/Exercise3Activity.java
@@ -2,12 +2,44 @@ package com.example.jmachahuay.lesson1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Exercise3Activity extends AppCompatActivity {
+
+    EditText edtNumberOfLines;
+    TextView txtLines;
+    Button btnCalculateNumberOfLine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise3);
+
+        edtNumberOfLines = (EditText) findViewById(R.id.edtNumberOfLines);
+        txtLines = (TextView) findViewById(R.id.txtLines);
+        btnCalculateNumberOfLine = (Button) findViewById(R.id.btnCalculateNumberOfLine);
+
+        btnCalculateNumberOfLine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String numberOfLinesText = edtNumberOfLines.getText().toString();
+
+                try {
+                    int txtNumberOfLines = Integer.parseInt(numberOfLinesText);
+
+                    txtLines.setText("");
+                    for (int i=0; i<txtNumberOfLines; i++){
+                        txtLines.append("Line " + (i+1) + "\n");
+                    }
+                }
+                catch (NumberFormatException ex) {
+                    Toast.makeText(Exercise3Activity.this, getResources().getString(R.string.txtValidNumber), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
